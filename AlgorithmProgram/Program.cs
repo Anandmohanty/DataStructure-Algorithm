c2ce738752c53e0f36a665b49f7fea24f5ba5016
Anandmohanty <mohantyanand3@gmail.com>
2023-01-23 11:16:49 +0530

    [Anand] - Binary Search

--- AlgorithmProgram/Program.cs
+++ AlgorithmProgram/Program.cs
@@ -1,18 +1,52 @@
-﻿namespace AlgorithmProgram
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+
+
+namespace AlgorithmProgram
 {
     internal class Program
     {
-        
-            static void Main(string[] args)
+        private static object Permutations;
+
+        public static object BinarySearch { get; private set; }
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Algorithm Programs!");
+            string flag = "Y";
+            while (flag == "Y" || flag == "y")
             {
-                Console.WriteLine("-----------------------------------------------------------------");
-                Console.WriteLine("Welcome to Data Structures & Algorithms Programminng");
-                Console.WriteLine("-----------------------------------------------------------------");
-                //*************************************************************************************
-                AlgorithmsProgramming algorithmsProgramming = new AlgorithmsProgramming();
-                algorithmsProgramming.algoProgram();
-                //*************************************************************************************
+                Console.WriteLine("1.Permutation Using Recursion");
+                Console.WriteLine("3.Binary Search the word from wordlist");
+                Console.WriteLine("Enter the option");
+                int num = Convert.ToInt32(Console.ReadLine());
+                switch (num)
+                {
+                    case 1:
+                        Console.WriteLine("Enter String to find Permutation ");
+                        string str = Console.ReadLine();
+                        int n = str.Length;
+                        Permutations.Permutate(str, 0, n - 1);
+                        break;
+                    case 2:
+                        string txtpath = File.ReadAllText(@"C:\Users\HP\source\repos\AlgorithmPrograms\AlgorithmPrograms\algo.txt");
+                        List<string> words = new(txtpath.Split());
+                        words.Sort();
+                        BinarySearch.BinarySearchWord(words);
+                        break;
+                    default:
+                        Console.WriteLine("Enter the valid option!");
+                        break;
+
+                }
+                Console.WriteLine("\nDo you want to continue");
+                flag = Console.ReadLine();
             }
         }
     }
-}
+    }
