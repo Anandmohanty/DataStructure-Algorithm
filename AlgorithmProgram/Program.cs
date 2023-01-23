adb1a53e0c14c094322a4c2bcf4f0fa8f678940a
Anandmohanty <mohantyanand3@gmail.com>
2023-01-23 10:33:51 +0530

    [Anand] - Algorithm program

---
+++ AlgorithmProgram/Program.cs
@@ -0,0 +1,18 @@
+﻿namespace AlgorithmProgram
+{
+    internal class Program
+    {
+        
+            static void Main(string[] args)
+            {
+                Console.WriteLine("-----------------------------------------------------------------");
+                Console.WriteLine("Welcome to Data Structures & Algorithms Programminng");
+                Console.WriteLine("-----------------------------------------------------------------");
+                //*************************************************************************************
+                AlgorithmsProgramming algorithmsProgramming = new AlgorithmsProgramming();
+                algorithmsProgramming.algoProgram();
+                //*************************************************************************************
+            }
+        }
+    }
+}
