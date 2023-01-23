76a1574128b4c71ff19b26e3b577b5cc407614a9
Anandmohanty <mohantyanand3@gmail.com>
2023-01-23 16:59:13 +0530

    [Anand] - Merge Shot

--- AlgorithmProgram/Program.cs
+++ AlgorithmProgram/Program.cs
@@ -3,16 +3,64 @@
     internal class Program
     {
         
+            
             static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Algorithm Programs!");
+            string flag = "Y";
+            while (flag == "Y" || flag == "y")
             {
-                Console.WriteLine("-----------------------------------------------------------------");
-                Console.WriteLine("Welcome to Data Structures & Algorithms Programminng");
-                Console.WriteLine("-----------------------------------------------------------------");
-                //*************************************************************************************
-                AlgorithmsProgramming algorithmsProgramming = new AlgorithmsProgramming();
-                algorithmsProgramming.algoProgram();
-                //*************************************************************************************
+                Console.WriteLine("1.Permutation Using Recursion");
+                Console.WriteLine("3.Binary Search the word from wordlist");
+                Console.WriteLine("4.Insertion Sort");
+                Console.WriteLine("5.Bubble Sort");
+                Console.WriteLine("6.Merge sort");
+                Console.WriteLine("Enter the option");
+                int num = Convert.ToInt32(Console.ReadLine());
+                switch (num)
+                {
+                    case 1:
+                        Console.WriteLine("Enter String to find Permutation ");
+                        string str = Console.ReadLine();
+                        int n = str.Length;
+                        Permutations.Permutate(str, 0, n - 1);
+                        break;
+                    case 2:
+                        string txtpath = File.ReadAllText(@"C:\Users\Radhika\source\repos\AlgorithmPrograms1\ListOfWords.txt");
+                        List<string> words = new List<string>(txtpath.Split(" "));
+                        words.Sort();
+                        BinarySearch.BinarySearchWord(words);
+                        break;
+                    case 3:
+                        InsertionSort insertionSort = new InsertionSort();
+                        int[] arr = { 10, 2, 14, 3, 1, 5, 9 };
+                        insertionSort.Sort(arr);
+                        insertionSort.PrintArray(arr);
+                        break;
+                    case 4:
+                        int[] arr1 = { 10, 2, 14, 3, 1, 5, 9 };
+                        BubbleSort.BubbleSortInArray(arr1);
+                        BubbleSort.PrintArray(arr1);
+                        break;
+                    case 5:
+                        int[] arr2 = { 32, 12, 9, 65, 24, 5 };
+                        MergeSort.Sort(arr2, 0, arr2.Length - 1);
+                        MergeSort.PrintArray(arr2);
+                        break;
+
+                    default:
+                        Console.WriteLine("Enter the valid option!");
+                        break;
+                }
+                Console.WriteLine("\nDo you want to continue");
+                flag = Console.ReadLine();
             }
+
+
+
+
+
+
         }
     }
 }
